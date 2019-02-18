dc5c5ec738f32a3f5c84b613bcc4d37408f98185
gblbjj <oliveira992gabriel@gmail.com>
2019-02-18 08:32:17 -0300

    segundo commit

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'teste5'
 maintainer 'Santander BR'
-maintainer_email 'you@example.com'
+maintainer_email 'oliveira992gabriel@gmail.com'
 issues_url	 'URL DO PROJETO /issues'
 source_url	 'URL DO PROJETO'
 privacy  true
 license 'all_rights'
 description 'Configures teste5'
-version '0.1.0'
+version '0.1.1'
